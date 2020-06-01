a9f6f06ff087c1b4ad41b95d05e83159c5ddd467
Sharif_Hosafy <Sherifismail44@gmail.com>
2020-06-01 14:19:35 +0200

    Adding BFS & DFS

--- Data Structures/BinarySearchTree.js
+++ Data Structures/BinarySearchTree.js
@@ -51,4 +51,62 @@ class BST {
       }
     }
   }
+
+  bfs() {
+    var Queue = [];
+    var visited = [];
+    Queue.push(this.root);
+
+    while (Queue.length) {
+      var current = Queue.shift();
+      visited.push(current.value);
+      if (current.left) Queue.push(current.left);
+      if (current.right) Queue.push(current.right);
+    }
+    return visited;
+  }
+
+  dfsPreOrder() {
+    var visited = [];
+    function travers(current) {
+      visited.push(current.value);
+      if (current.left) travers(current.left);
+      if (current.right) travers(current.right);
+    }
+    travers(this.root);
+    return visited;
+  }
+
+  dfsPostOrder() {
+    var visited = [];
+    function travers(current) {
+      if (current.left) travers(current.left);
+      if (current.right) travers(current.right);
+      visited.push(current.value);
+    }
+    travers(this.root);
+    return visited;
+  }
+
+  dfsInOrder() {
+    var visited = [];
+    function travers(current) {
+      if (current.left) travers(current.left);
+      visited.push(current.value);
+      if (current.right) travers(current.right);
+    }
+    travers(this.root);
+    return visited;
+  }
 }
+
+var bst = new BST();
+bst.insert(10);
+bst.insert(6);
+bst.insert(15);
+bst.insert(3);
+bst.insert(8);
+bst.insert(20);
+console.log("pre " + bst.dfsPreOrder());
+console.log("post " + bst.dfsPostOrder());
+console.log("in " + bst.dfsInOrder());
